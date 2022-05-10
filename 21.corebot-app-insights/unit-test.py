c494cb2d89d1708f6367c55d0de3f4a11f3bb33b
raoofrachidi <raoof976@gmail.com>
2022-05-11 00:06:46 +0200

    change test

--- 21.corebot-app-insights/unit-test.py
+++ 21.corebot-app-insights/unit-test.py
@@ -1,10 +1,69 @@
-from booking_details import BookingDetails
+from azure.cognitiveservices.language.luis.runtime import LUISRuntimeClient
+from msrest.authentication import CognitiveServicesCredentials
+from config import DefaultConfig
 
-def test_flight_or_city():
-    assert BookingDetails(or_city='Paris').or_city == "Paris"
+CONFIG = DefaultConfig()
 
-def test_flight_dst_city():
-    assert BookingDetails(dst_city='London').dst_city == "London"
+# Create request
+request = 'looking to go from san francisco to marseille. book me for september 18 to 22. let me know if its more ' \
+          'than 2800 because thats all i can afford '
 
-def test_flight_budget():
-    assert BookingDetails(or_city=1000).or_city == 1000
+
+def test_luis_intent():
+    """Check LUIS non-regression on *Top intent*
+    """
+    # Instantiate prediction client
+    clientRuntime = LUISRuntimeClient(
+        CONFIG.LUIS_API_HOST_NAME,
+        CognitiveServicesCredentials(CONFIG.LUIS_API_KEY))
+
+    # Get response
+    response = clientRuntime.prediction.resolve(CONFIG.LUIS_APP_ID, query=request)
+
+    check_top_intent = 'BookFlightIntent'
+    is_top_intent = response.top_scoring_intent.intent
+    assert check_top_intent == is_top_intent
+
+
+def test_luis_origin():
+    """Check LUIS non-regression on *Origin*
+    """
+    # Instantiate prediction client
+    clientRuntime = LUISRuntimeClient(
+        CONFIG.LUIS_API_HOST_NAME,
+        CognitiveServicesCredentials(CONFIG.LUIS_API_KEY))
+
+    # Get response
+    response = clientRuntime.prediction.resolve(CONFIG.LUIS_APP_ID, query=request)
+
+    check_origin = 'san francisco'
+    all_entities = response.entities
+    is_origin = None
+
+    for i in range(0, len(all_entities)):
+        if all_entities[i].type == 'or_city':
+            is_origin = all_entities[i].entity
+
+    assert check_origin == is_origin
+
+
+def test_luis_destination():
+    """Check LUIS non-regression on *Destination*
+    """
+    # Instantiate prediction client
+    clientRuntime = LUISRuntimeClient(
+        CONFIG.LUIS_API_HOST_NAME,
+        CognitiveServicesCredentials(CONFIG.LUIS_API_KEY))
+
+    # Get response
+    response = clientRuntime.prediction.resolve(CONFIG.LUIS_APP_ID, query=request)
+
+    check_destination = 'marseille'
+    all_entities = response.entities
+    is_destination = None
+
+    for i in range(0, len(all_entities)):
+        if all_entities[i].type == 'dst_city':
+            is_destination = all_entities[i].entity
+
+    assert check_destination == is_destination
