175f3adbe43d087a61ca32dc6d8cb7630178817b
raoofrachidi <raoof976@gmail.com>
2022-05-12 18:17:33 +0200

    fix

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class DefaultConfig:
     LUIS_APP_ID = os.environ.get("LuisAppId", "476ae015-dac4-44ce-9dcc-ca1da0154e3b")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "79b60d071a594d15b2a83e30ac9dc8c4")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/476ae015-dac4-44ce-9dcc-ca1da0154e3b?subscription-key=79b60d071a594d15b2a83e30ac9dc8c4&q=")
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "chatbotluisoc-authoring.cognitiveservices.azure.com")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
         "AppInsightsInstrumentationKey", "e3919f29-1500-4f1b-a174-1f5fc46b8715"
     )
